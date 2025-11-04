38f2bf3223814b3db120f340fcfb08a837309547
Asad Siddiqui <asiddiqui@sesimi.com>
2025-11-03 19:00:38 -0500

    Icon changing on URL/Tab

--- background.js
+++ background.js
@@ -1,25 +1,35 @@
-chrome.tabs.onActivated.addListener(function (activeInfo) {
-  if (tab.url.includes("sesimi.app") || tab.url.includes("myadboxapp")) {
-    document.getElementById("tabTitle").textContent = `danger`;
-    chrome.action.setIcon({ path: "./danger.png" });
-  } else if (tab.url.includes("sesimi.io") || tab.url.includes("adboxapp")) {
-    document.getElementById("tabTitle").textContent = `warning`;
-    chrome.action.setIcon({ path: "./warning.png" });
-  } else {
-    document.getElementById("tabTitle").textContent = `safe`;
-    chrome.action.setIcon({ path: "./safe.png" });
-  }
+// Listen for when the active tab changes within the current window
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  updateIcon();
 });
 
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (tab.url.includes("sesimi.app") || tab.url.includes("myadboxapp")) {
-    document.getElementById("tabTitle").textContent = `danger`;
-    chrome.action.setIcon({ path: "./danger.png" });
-  } else if (tab.url.includes("sesimi.io") || tab.url.includes("adboxapp")) {
-    document.getElementById("tabTitle").textContent = `warning`;
-    chrome.action.setIcon({ path: "./warning.png" });
-  } else {
-    document.getElementById("tabTitle").textContent = `safe`;
-    chrome.action.setIcon({ path: "./safe.png" });
+// Listen for when a tab's content finishes loading (e.g., navigation to a new page within the same tab)
+// and its properties like URL change
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (tab && changeInfo.status === "complete" && tab.active) {
+    updateIcon();
   }
 });
+async function updateIcon() {
+  let icon = "";
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  if (
+    tab.url.includes("chrome://") ||
+    tab.url.includes("edge://") ||
+    tab.url.includes("about:")
+  ) {
+    chrome.action.setIcon({ path: `./safe.png`, tabId: tab.id });
+    return;
+  }
+  if (tab.url.includes("sesimi.app") || tab.url.includes("myadboxapp")) {
+    icon = "danger";
+  } else if (tab.url.includes("sesimi.io") || tab.url.includes("adboxapp")) {
+    icon = "warning";
+  } else {
+    icon = "safe";
+  }
+  chrome.action.setIcon({ path: `./${icon}.png`, tabId: tab.id });
+}
+
+updateIcon();
--- content.js
+++
@@ -1,8 +0,0 @@
-// Example: Change the background color of the active tab's body
-document.body.style.backgroundColor = "lightblue";
-
-// Example: Log the page title to the console
-console.log("Active tab title:", document.title);
-
-// You can also send messages back to the background script or popup
-chrome.runtime.sendMessage({ action: "pageAnalyzed", title: document.title });
