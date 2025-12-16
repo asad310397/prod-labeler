a3c320903e51117babbebfca0aa85b98ce76d6dd
Asad Siddiqui <asiddiqui@sesimi.com>
2025-12-16 14:21:34 -0500

    add ui for accordion and lists

--- scripts/url_list.js
+++ scripts/url_list.js
@@ -29,6 +29,24 @@ const addUrl = () => {
   }
 };
 
+const createElement = (list, url) => {
+  const li = document.createElement("li");
+  li.textContent = url;
+  const button = document.createElement("button");
+  button.classList.add("remove-button");
+  button.textContent = "x";
+  button.addEventListener("click", () => {
+    chrome.storage.sync.get([list], (data) => {
+      const updatedUrls = data[list].filter((item) => item !== url);
+      chrome.storage.sync.set({ [list]: updatedUrls }, () => {
+        populateUrls();
+      });
+    });
+  });
+  li.appendChild(button);
+  return li;
+};
+
 const populateUrls = () => {
   const dangerList = document.getElementById("danger-list");
   const warningList = document.getElementById("warning-list");
@@ -39,16 +57,16 @@ const populateUrls = () => {
   chrome.storage.sync.get(["dangerUrls", "warningUrls"], (data) => {
     if (data.dangerUrls) {
       data.dangerUrls.forEach((url) => {
-        const li = document.createElement("li");
-        li.textContent = url;
-        dangerList.appendChild(li);
+        let item = createElement("dangerUrls", url);
+        console.log(item);
+        dangerList.appendChild(item);
       });
     }
     if (data.warningUrls) {
       data.warningUrls.forEach((url) => {
-        const li = document.createElement("li");
-        li.textContent = url;
-        warningList.appendChild(li);
+        let item = createElement("warningUrls", url);
+        console.log(item);
+        warningList.appendChild(item);
       });
     }
   });
@@ -69,6 +87,20 @@ function loadLinks() {
 const initialize = () => {
   loadLinks();
   document.getElementById(`add`).addEventListener("click", () => addUrl());
+
+  const accordions = document.getElementsByClassName("accordion-section");
+  Array.from(accordions).forEach((item) => {
+    item
+      .getElementsByClassName("accordion-title")[0]
+      .addEventListener("click", (e) => {
+        let classes = item.classList;
+        if (classes.contains("active")) {
+          item.classList.remove("active");
+        } else {
+          item.classList.add("active");
+        }
+      });
+  });
 };
 
 document.addEventListener("DOMContentLoaded", initialize);
